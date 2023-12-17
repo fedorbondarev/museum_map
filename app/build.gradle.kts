58385878dd3e60347820a5bc4ab6b8532d4b91de
Daniil <drobucs@gmail.com>
2023-12-17 19:56:28 +0300

    layout for map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("com.davemorrissey.labs:subsampling-scale-image-view:3.10.0")
 }
 
 kapt {
