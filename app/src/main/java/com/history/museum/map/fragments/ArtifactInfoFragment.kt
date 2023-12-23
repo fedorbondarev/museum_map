0cc047393a724b79a9b41cf1a2a696db7630e12e
Fedor Bondarev <fedorbondarev@outlook.com>
2023-12-23 19:36:45 +0300

    Added content to artifact info

--- app/src/main/java/com/history/museum/map/fragments/ArtifactInfoFragment.kt
+++ app/src/main/java/com/history/museum/map/fragments/ArtifactInfoFragment.kt
@@ -1,19 +1,19 @@
 package com.history.museum.map.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.history.museum.map.R
-import com.history.museum.map.data.models.entities.ArtifactEntity
 
 class ArtifactInfoFragment : Fragment(R.layout.artifact_info_fragment) {
     private val args: ArtifactInfoFragmentArgs by navArgs()
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -22,6 +22,11 @@ class ArtifactInfoFragment : Fragment(R.layout.artifact_info_fragment) {
             requireActivity().onBackPressedDispatcher.onBackPressed()
         }
 
-        view.findViewById<TextView>(R.id.example_text).text = args.artifact.name
+        view.findViewById<TextView>(R.id.title).text = args.artifact.name
+
+
+        val webView = view.findViewById<WebView>(R.id.info_web_view)
+        webView.settings.javaScriptEnabled = true
+        webView.loadData(args.artifact.html, "text/html; charset=utf-8", "UTF-8")
     }
 }
