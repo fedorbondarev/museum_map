960cdc2ef1dc53e58901eaaf030e1ca129cf9f27
Daniil <drobucs@gmail.com>
2023-12-24 18:18:08 +0300

    add artifacts 10, 11, 12

--- app/src/main/java/com/history/museum/map/data/DataBase.kt
+++ app/src/main/java/com/history/museum/map/data/DataBase.kt
@@ -4,6 +4,9 @@ import com.history.museum.map.data.models.Point
 import com.history.museum.map.data.models.Triangle
 import com.history.museum.map.data.models.entities.ArtifactEntity
 import com.history.museum.map.data.models.entities.InfoEntity
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 
 fun createTrianglesForRect(x: Double, y: Double, w: Double, h: Double): List<Triangle> {
@@ -11,6 +14,26 @@ fun createTrianglesForRect(x: Double, y: Double, w: Double, h: Double): List<Tri
         Triangle(Point(x, y), Point(x + w, y), Point(x + w, y + h)),
         Triangle(Point(x, y), Point(x, y + h), Point(x + w, y + h))
     )
+}
+
+fun circleTriangles(n: Int, radius: Double, center: Point): List<Triangle> {
+    val angle = 2 * PI / n
+    val points = mutableListOf<Point>()
+    var v = Point(0.0, radius)
+    for (i in 0..n) {
+        points.add(center + v)
+        val x = v.x * cos(angle) - v.y * sin(angle)
+        val y = v.x * sin(angle) + v.y * cos(angle)
+        v = Point(x, y)
+    }
+    points.add(center + v)
+    val triangles = mutableListOf<Triangle>()
+    for (i in 0 until points.size) {
+        triangles.add(
+            Triangle(center, points[i % points.size], points[(i + 1) % points.size])
+        )
+    }
+    return triangles
 }
 
 class DataBase {
@@ -63,6 +86,49 @@ class DataBase {
                     </html>
                 """.trimIndent(),
                 createTrianglesForRect(1182.0, 414.0, 279.0, 1767.0)
+            ),
+            ArtifactEntity(
+                10,
+                "Экспонат 10",
+                2,
+                """
+                    <html>
+                    <body>
+                        <p>Добро пожаловать на нашу выставку музея! Исследуйте увлекательные артефакты и узнайте о их истории и значимости.</p>
+                        <p>Не забудьте посетить наши интерактивные дисплеи и принять участие в практических занятиях.</p>
+                    </body>
+                    </html>
+                """.trimIndent(),
+                createTrianglesForRect(830.0, 700.0, 160.0, 261.0) +
+                        createTrianglesForRect(771.0, 956.0, 270.0, 150.0)
+            ),
+            ArtifactEntity(
+                11,
+                "Экспонат 11",
+                2,
+                """
+                    <html>
+                    <body>
+                        <p>Добро пожаловать на нашу выставку музея! Исследуйте увлекательные артефакты и узнайте о их истории и значимости.</p>
+                        <p>Не забудьте посетить наши интерактивные дисплеи и принять участие в практических занятиях.</p>
+                    </body>
+                    </html>
+                """.trimIndent(),
+                circleTriangles(12, 140.0, Point(910.0, 1299.0))
+            ),
+            ArtifactEntity(
+                12,
+                "Экспонат 12",
+                2,
+                """
+                    <html>
+                    <body>
+                        <p>Добро пожаловать на нашу выставку музея! Исследуйте увлекательные артефакты и узнайте о их истории и значимости.</p>
+                        <p>Не забудьте посетить наши интерактивные дисплеи и принять участие в практических занятиях.</p>
+                    </body>
+                    </html>
+                """.trimIndent(),
+                createTrianglesForRect(1051.0, 2796.0, 409.0, 181.0)
             )
         )
     }
